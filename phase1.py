74e5f2aa9cac36a0b5e28c159d4c789a2c41d7e1
Soufyan Belkaid <s.belkaid@student.vu.nl>
2017-01-29 11:24:14 +0100

    phases

--- phase1.py
+++ phase1.py
@@ -26,58 +26,70 @@ How to Use the Data
    - use your algorithm to compare these pairs to the paradigms or definitions 
      from the previous step and rate the word pairs in Phase1Answers according 
      to their degree of prototypicality for the given relation
-
-3. Phase2Questions 
-   - answer these MaxDiff questions, using your ratings from the preceding
-     step
-   - see the Perl scripts for examples
-
-4. Phase2Answers
-   - compare your answers to human answers
-   - see the Perl scripts for examples
 '''
 
 import sys
 import os
+import numpy as np
 
 
-input_folder = '/Users/soufyanbelkaid/Research/degrees_of_similarities/SemEval-2012-Complete-Data-Package/Testing/Phase1Answers'
-path_to_definitions = '/Users/soufyanbelkaid/Research/degrees_of_similarities/SemEval-2012-Complete-Data-Package/subcategories-list.txt'
+input_folder_testing = 'SemEval-2012-Complete-Data-Package/Testing/Phase1Answers'
+input_folder_training = 'SemEval-2012-Complete-Data-Package/Training/Phase1Answers'
+path_to_definitions = 'SemEval-2012-Complete-Data-Package/subcategories-list.txt'
 
-categorie_file = [e.split(',') for e 
+category_file = [e.split(',') for e 
     in open(path_to_definitions, 'r').read().split('\n')]
-print categorie_file
+CATEGORY_FILE = {''.join([e[0], e[1]]).replace(" ", ""):e[2]+'-'+e[3] for e in category_file[:-1]}
 
 
-def do_ranking():
+def do_ranking(category, word_pairs):
     """
     This function should calculate something and rank the pairs.
-    """
-    pass
+    :param category: the category of the word pairs
+    :type category: str
+    :param word_pairs: list of word pairs
+    :type word_pairs: list
+    """  
+    rand_vals = np.random.uniform(-1, 100, size=len(word_pairs)) #random numbers
+    ranked = sorted(zip(rand_vals, word_pairs), key=lambda x:x[0], reverse=True) #sorting them
+    formatted = []
+    for e in ranked:
+      prob = '%.1f'%e[0]
+      formatted.append('{} "{}:{}"'.format(prob, e[1][0],e[1][1]))
+    return formatted
 
-
-def do_som_with_pairs(categorie, word_pairs):
+  
+def do_som_with_pairs(category, word_pairs):
     """
     This function takes the word pairs provided by the Turkers
-    as input and has to rank them from most similar to least. The function
-    should call another function that calculates the similarity.
+    as input and has to rank them from most similar to least for a given category. The function
+    should call another function that calculates the rank probability.
     Here the model trained should be summoned. 
+    The output is written to a file in OUTPUT_PHASE1 directory
     :param categorie: str, representing the categorie. Explanation can
     be found in subcategories.txt
     :param word_pairs: list(list), containing word pairs provided by annotators 
     """
-    print categorie, word_pairs[0]
+    # print "categorie:", category, word_pairs[0] # temporaril only printing first pair
+    calculated_and_ranked = do_ranking(category, word_pairs)
+    with open('OUTPUT_PHASE1/'+category+'-scaled.txt', 'w') as f:
+      for pair_string in calculated_and_ranked:
+        f.write('%s\n' % pair_string)
 
 
-def determine_categorie(path_to_answers):
-    file_names = os.listdir(path_to_answers)
+def start(path_to_answers_training, path_to_answers_testing):
+    file_names = [(path_to_answers_training, e) for e in os.listdir(path_to_answers_training)]
+    file_names.extend([(path_to_answers_testing, e) for e in os.listdir(path_to_answers_testing)]) # does not matter that it is testing, just covering all pairs and categories
+    if not os.path.exists('OUTPUT_PHASE1'):
+      os.mkdir('OUTPUT_PHASE1')
     for fn in file_names:
-        categorie_id = fn[fn.find('-')+1:fn.find('.')]
-        raw_pairs = open(path_to_answers+'/'+fn, 'r').read().split('\n')
-        word_pairs = [e.strip('"').split(':') for e in raw_pairs]
-        do_som_with_pairs(categorie_id, word_pairs)
+      path, fn = fn[0]
+      categorie_id = fn[fn.find('-')+1:fn.find('.')] #extracting categorie of file
+      raw_pairs = open(os.path.join(path,fn), 'r').read().split('\n')
+      word_pairs = [e.strip('"').split(':') for e in raw_pairs if e]
+      do_som_with_pairs(categorie_id, word_pairs)
 
 
 if __name__ == '__main__':
-    determine_categorie(input_folder)
+    start(input_folder_training, input_folder_testing)
 
---
+++ phase2.py
@@ -0,0 +1,108 @@
+"""
+
+This script takes the output of phase1 and the questions (maxdiff) of Phase2
+
+The category labels will be visible in the training set but hidden in the testing set.
+ The systems must guess the categories of the pairs in the testing set and indicate the degree 
+ to which each pair is a prototypical member of the guessed category.
+
+3. Phase2Questions 
+   - answer these MaxDiff questions, using your ratings from the preceding
+     step
+   - see the Perl scripts for examples
+
+4. Phase2Answers
+   - compare your answers to human answers
+   - see the Perl scripts for examples
+"""
+
+import sys
+import os
+
+IN_FOL_P1 = os.listdir('OUTPUT_PHASE1')
+# path_to_fol = sys.argv[1]
+definitions_file = 'SemEval-2012-Complete-Data-Package/subcategories-list.txt'
+path_to_questions = 'SemEval-2012-Complete-Data-Package/Training/Phase2Questions'
+category_file = [e.split(',') for e 
+    in open(definitions_file, 'r').read().split('\n')]
+CATEGORY_FILE = {''.join([e[0], e[1]]).replace(" ", ""):e[2]+'-'+e[3] for e in category_file[:-1]}
+DIR_OUTPUT_PHASE1 = 'OUTPUT_PHASE1'
+
+
+def create_ranking_dict():
+	ranked_lists = os.listdir(DIR_OUTPUT_PHASE1)
+	aggregated = {}
+	for fn in ranked_lists:
+		category_id = fn[:fn.find('-')] # for instance 9e 
+		aggregated[category_id] = {}
+		ranked_lines = open(os.path.join(DIR_OUTPUT_PHASE1, fn)).read().split('\n')
+		for line in ranked_lines:
+			if line:
+				score, word_pair = line.split()
+				aggregated[category_id][word_pair] = score
+	return aggregated
+
+
+def get_ranking(word_pairs, relation_category):
+	"""
+	This function ranks the 4 from most to least illustrative for a given
+	semantic relation.
+	:param word_pairs: list of 4 word word pairs
+	:type word_pairs: list(str)
+	:param relation_category: category of the word_pairs
+	:type relation_category:str
+	"""
+	sub_dict = ranking_dict[relation_category]
+	rank = []
+	for pair in word_pairs:
+		score = float(sub_dict[pair])
+		rank.append((score, pair))
+	return sorted(rank, key=lambda x:x[0], reverse=True)
+
+
+
+
+def read_questions(question_str, relation_category):
+	"""
+	Extracts the pairs and call the get_ranking function. Then
+	format them correctly. 
+	"""
+	each_line = question_str.split('\n')
+	for pair in each_line:
+		if pair:
+			four_pairs = pair.split(',')
+			print get_ranking(four_pairs, relation_category)
+			print '\n'
+
+
+
+
+
+
+def start(question_dir_path):
+	"""
+	This function opens and parses the questions. 
+	:param question_dir_path: path
+	:type question_dir_path: str
+
+	"""
+	
+	file_names = os.listdir(question_dir_path)	
+	for fn in file_names:
+		print fn
+		category_id = fn[fn.find('-')+1:fn.find('.')]
+		source = open(os.path.join(question_dir_path, fn), 'r').read()
+		read_questions(source, category_id)
+		break
+
+
+
+
+if __name__ == '__main__':
+	ranking_dict = create_ranking_dict()
+	start(path_to_questions)
+	
+
+
+
+
