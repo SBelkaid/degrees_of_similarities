1fd342563dabadd4e6bf760166c6461f0658f6ad
Soufyan Belkaid <s.belkaid@student.vu.nl>
2017-02-02 14:05:07 +0100

    .

--- evaluate.py
+++ evaluate.py
@@ -29,8 +29,11 @@ def run_eval(cmd):
 def format_eval(input_fol, gold_input_fol, eval_script = None):
 	"""
 	Run perl evaluation scripts on all the files through subprocess.
-	:param input_fol: path to folder with phase1 output files
-	:type input_fol: str	
+	
+	:param input_fol: path to folder with output files either phase 1 or phase 2
+	:type input_fol: str
+	:param gold_input_fol: path to folder with scale or maxdiff output files
+	:type gold_input_fol: str
 	"""
 	output_file_ext = eval_script.split('_')[1]
 	path_to_eval_scripts = 'SemEval-2012-Complete-Data-Package/'
