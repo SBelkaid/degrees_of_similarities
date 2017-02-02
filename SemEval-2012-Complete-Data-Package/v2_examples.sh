e28184e61ddee883977e3dad02945926a57a8dd8
Soufyan Belkaid <s.belkaid@student.vu.nl>
2017-02-02 11:30:48 +0100

    .

--- SemEval-2012-Complete-Data-Package/v2_examples.sh
+++ SemEval-2012-Complete-Data-Package/v2_examples.sh
@@ -10,7 +10,13 @@ echo ----------------------
 echo MAKING RESULT DIRECTORY
 echo ----------------------
 #
-mkdir ../RESULTS
+
+output_folder="../RESULTS"
+
+if [ ! -d $output_folder ]; then
+	mkdir $output_folder
+fi
+
 #
 #   score_scale.pl <input file of Gold Standard pair ratings> 
 #                  <input file of pair ratings to be evaluated> <output file of results>
@@ -37,6 +43,5 @@ echo ------------------------
 #
 perl score_maxdiff.pl Training/Phase2Answers/Phase2Answers-1a.txt \
                  ../OUTPUT_PHASE2/1a-MaxDiff.txt ../RESULTS/ResultMaxDiffRandom-1a.txt
-#
-#
-#
+
+
